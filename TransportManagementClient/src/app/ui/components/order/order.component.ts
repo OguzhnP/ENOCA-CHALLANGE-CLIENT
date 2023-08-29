195b9f38061efbe3eca495ddf3056164c5c9655a
Oguzhan Portakal <101134423+OguzhnP@users.noreply.github.com>
2023-08-29 04:43:51 +0300

    added alertify service

--- TransportManagementClient/src/app/ui/components/order/order.component.ts
+++ TransportManagementClient/src/app/ui/components/order/order.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit{
+
+  constructor(
+    private alertify : AlertifyService
+  ){}
+  ngOnInit(): void {
+    this.alertify.message("deneme",MessageType.Warning);
+  }
 
 }
